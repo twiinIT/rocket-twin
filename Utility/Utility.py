1fc17b9b2dd87b8abe51af93e3b84ae57083eb28
Gauthier <avite.gauthier@gmail.com>
2023-02-22 15:14:27 +0100

    Refactoring into folders

--- Utility/Utility.py
+++ Utility/Utility.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-###THRUST
-thrust_data = pd.read_csv("thrust.txt", header=None)
+thrust_data = pd.read_csv("Utility/thrust.txt", header=None)
 times = list(thrust_data.iloc[0])
 thrusts = list(thrust_data.iloc[1])
 
