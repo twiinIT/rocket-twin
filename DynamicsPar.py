9216ee4a5d18023625bd5c53f192c4ea65577b6d
Gauthier Avite <avite.gauthier@gmail.com>
2023-04-02 00:02:01 +0200

    debugging

--- DynamicsPar.py
+++ DynamicsPar.py
@@ -1,7 +1,6 @@
 from cosapp.base import System
 
 from Ports import VelPort, AclPort
-from Utility.Utility import thrust
 
 import numpy as np
 
@@ -16,8 +15,8 @@ class DynamicsPar(System):
         
         self.add_inward('l0', 1., desc = "Rope rest length", unit = 'm')
         self.add_inward('k', 100., desc = "rope's stiffness", unit='N/m')
-        self.add_inward('m1', 1.6, desc = "Mass of parachute + nosecone", unit = 'kg')
-        self.add_inward('m2', 10., desc = "Mass of rocket - nosecone", unit = 'kg')
+        self.add_inward('m1', 1., desc = "Mass of parachute + nosecone", unit = 'kg')
+        self.add_inward('m2', 2., desc = "Mass of rocket - nosecone", unit = 'kg')
         self.add_inward('S_ref', .1, desc = "Reference surface of parachute", unit = 'm**2')
         self.add_inward('Cd', 1.75, desc = "Drag coefficient of parachute", unit = '')
         self.add_inward('r_in', np.zeros(3), desc = "Rocket Position", unit = 'm')
@@ -45,51 +44,28 @@ class DynamicsPar(System):
 
         if self.ParachuteDeployed.present:
 
-            print("R1 ABOUGA")
-            print(self.r1)
-            self.Dep = 1
-            l0 = [self.l0,0,0]
-            rotation = R.from_euler('xyz', self.ang, degrees=False)
-            vect1 = rotation.apply(l0)
-            self.r1 = self.r1 + vect1
+            print("PARACHUTE DEPLOYMENT")
+            self.r1 = np.array([1.,0.,500.])
+            self.r2 = np.array([0.,0.,500.])
             print(self.r1, self.r2)
-            print("R1 ABOUGA")
+            self.Dep = 1
+            # l0 = [self.l0,0,0]
+            # rotation = R.from_euler('xyz', self.ang, degrees=False)
+            # vectEarth = rotation.apply(l0)
+            # self.r1 = self.r1 + vectEarth
+            print(self.r1, self.r2)
+            print(self.v1, self.v2)
+            print("PARACHUTE DEPLOYMENT")
 
 
         
     def compute(self):
 
         if self.Dep == 0:
-            print("not deployed yet")
-            print("r_in")
-            print(self.r_in)
-            print("angles")
-            print(self.ang)
-            self.v1 = np.zeros(3)
-            self.v2 = np.zeros(3)
-            self.r1 = np.array([0.,0.,100.])
-            self.r2 = np.array([0.,0.,100.])
-
-            print("r1")
-            print(self.r1)
-            print("r2")
-            print(self.r2)
-            print("v1")
-            print(self.v1)
+            pass
 
         else:
-            print("deployed")
-            print("r1")
-            print(self.r1)
-            print("r2")
-            print(self.r2)
-            print("v1")
-            print(self.v1)
             Drag = -.5 * self.S_ref * self.Cd * np.linalg.norm(self.v1) * (self.v1-self.v_wind.val) 
-            print("Drag")
-            print(Drag)
-            print(self.v2)
             d = -self.r2 + self.r1
-            # d_norm = 0 if np.linalg.norm(d)<1e-6 else 
             self.a1 = -(self.k / self.m1) * (d-self.l0*d/np.linalg.norm(d)) + np.array([0.,0.,-9.8]) + Drag/self.m1
             self.a2 = -(self.k / self.m2) * (-d+self.l0*d/np.linalg.norm(d)) + np.array([0.,0.,-9.8])
