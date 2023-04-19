23b18d9d127cf62cbb36efb31f7bb2079e18224a
Luca Miguel <luca.miguel@usp.br>
2023-04-19 15:46:31 +0200

    Gathered necessary data for model validation

--- UI/model/Trajectory.py
+++ UI/model/Trajectory.py
@@ -23,8 +23,8 @@ class Trajectory(System):
         self.add_inward('l0', 1., desc = "Rope rest length", unit = 'm')
 
         #Event
-        self.add_event("ParachuteDeployed", trigger='v.val[2] < 0')
-        # self.add_event("ParachuteDeployed", trigger = 'time > 9.')
+        self.add_event("Apogee", trigger='v.val[2] < 0')
+        self.add_event("ParachuteDeployed", trigger = 'time > 9.')
         self.add_inward("apogee_time", np.Infinity, unit = "s")
         self.add_event("FinallyDeployed", trigger = "time > apogee_time + .1 ") # The parachute takes .1 second to deploy itself
 
@@ -32,9 +32,24 @@ class Trajectory(System):
 
     def transition(self):
 
+        if self.Apogee.present and self.ParaDep  == 0:
+
+            print('\n')
+            print("___APOGEE___")
+            print('\n')
+            print("Flight Time at Apogee: ", self.time, 's')
+            print('\n')
+            print("X Coordinate at Apogee: ", self.r[0], 'm')
+            print('\n')
+            print("Y Coordinate at Apogee: ", self.r[1], 'm')
+            print('\n')
+            print("Z Coordinate at Apogee: ", self.r[2], 'm')
+            print('\n')
+
         if self.ParachuteDeployed.present and self.ParaDep == 0:
 
             print("___PARACHUTE DEPLOYMENT___")
+            print('\n')
             self.apogee_time = self.time
             DynPar = self.parent.Para.DynPar
             DynPar.r1 = self.r
@@ -45,8 +60,22 @@ class Trajectory(System):
             rotation = R.from_euler('xyz', self.ang, degrees=False)
             vectEarth = rotation.apply(l0)
             DynPar.r1 = DynPar.r1 + vectEarth
-            print("coordinates (1,2) at apogee", DynPar.r1, DynPar.r2)
-            print("velocity (1,2) at apogee", DynPar.v1, DynPar.v2)
+            #print("coordinates (1,2) at apogee", DynPar.r1, DynPar.r2)
+            #print("velocity (1,2) at apogee", DynPar.v1, DynPar.v2)
+            #print('Apogee time', self.time)
+
+            print("Point 1 X Coordinate at Deployment: ", DynPar.r1[0], 'm')
+            print('\n')
+            print("Point 1 Y Coordinate at Deployment: ", DynPar.r1[1], 'm')
+            print('\n')
+            print("Point 1 Z Coordinate at Deployment: ", DynPar.r1[2], 'm')
+            print('\n')
+            print("Point 2 X Coordinate at Deployment: ", DynPar.r2[0], 'm')
+            print('\n')
+            print("Point 2 Y Coordinate at Deployment: ", DynPar.r2[1], 'm')
+            print('\n')
+            print("Point 2 Z Coordinate at Deployment: ", DynPar.r2[2], 'm')
+            print('\n')
             print("___PARACHUTE DEPLOYMENT___")
             
         
