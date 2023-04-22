8eebfc35c4e92e995dfca76a59fbe03a067f31f0
Gauthier Avite <avite.gauthier@gmail.com>
2023-04-22 20:18:58 +0200

    plot improvement

--- fusee.py
+++ fusee.py
@@ -90,7 +90,6 @@ driver2.set_scenario(
 	stop = 'z <= 0.' #On arrete la simulation quand la fusée tombe au sol
 	)
 
-
 fusee.run_drivers()
 fusee2.run_drivers()
 
@@ -104,115 +103,16 @@ data2 = data2.drop(['Section', 'Status', 'Error code'], axis=1)
 time2 = np.asarray(data2['time'])
 traj2 = np.asarray(data2['z'].tolist())
 
-
-import plotly.graph_objects as go
-
 length = traj.size
 
-#0 en x et y (#1D)
-x = np.array([0 for i in range (0,length)])
+import matplotlib.pyplot as plt
 
+plt.plot(time, traj, 'r', label = "Masse de gaz : 1 kg")
+plt.plot(time2, traj2, 'b', label = "Masse de gaz : 7 kg")
 
-#Create Figure
-figure1 = go.Figure(data=[go.Scatter3d(x=x, 
-										y=x, 
-										z=traj, 
-										mode="lines", 
-										line=dict(width=2, color="blue")
-										),
-						],
-					)
+plt.title("Trajectoire d'une fusée de 2kg à vide, débit massique de 1.0kg/s,\n soumise à son poids et à une force de poussée")
+plt.xlabel("time"),
+plt.ylabel("altitude")
 
-
-
-
-#Frames 
-frames=[go.Frame(data=[go.Scatter3d(x=[x[k]],
-									y=[x[k]],
-									z=[traj[k]],
-									mode="markers",
-									marker={"color" : "blue", "size" : 10, "symbol" : "diamond"}
-									)
-						]
-				)
-	for k in range(length)]
-
-
-figure1.update(frames=frames)
-
-
-def frame_args(duration):
-    return {
-            "frame": {"duration": duration},
-            "mode": "immediate",
-            "fromcurrent": True,
-            "transition": {"duration": 0},
-            }
-
-
-#Buttons, frame durations
-figure1.update_layout(
-					updatemenus = [{"buttons":[
-													{
-														"args" : [None, frame_args(20)],
-														"label" : "Play",
-														"method" : "animate",
-
-													},
-													{
-														"args" : [[None], frame_args(0)],
-														"label" : "Pause",
-														"method" : "animate",
-													}
-												]
-
-
-									}]
-					)
-
-
-
-#Scale 
-figure1.update_layout(scene = dict(xaxis=dict(range=[min(x) - 2, max(x) + 2], autorange=False),
-                               yaxis=dict(range=[min(x) - 2, max(x) + 2], autorange=False),
-                               zaxis=dict(range=[min(traj), max(traj)], autorange=False)
-                               )
-                  )
-
-
-#Figure 2
-
-figure2 = go.Figure(data=[go.Scatter(x=time, 
-										y=traj, 
-										mode="lines", 
-										line=dict(width=1, color="red"),
-										name="Masse de gaz : 7kg"
-										),
-						] + [go.Scatter(x=time2, 
-										y=traj2, 
-										mode="lines", 
-										line=dict(width=1, color="blue"),
-										name="Masse de gaz : 1kg"
-										)],
-					)
-figure2.update_layout(
-    title = "Trajectoire d'une fusée de 2kg à vide, débit massique de 1.0kg/s, soumise à son poids et à une force de poussée",
-    xaxis = dict(title="time"),
-    yaxis = dict(
-        title = "altitude",
-    )
-)
-
-
-
-
-#figure1.show()
-figure2.show()
-
-
-
-
-
-
-
-
+plt.legend()
+plt.show()
