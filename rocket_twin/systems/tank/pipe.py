36884f6760653118f02f0f5c555e8d1fc866a80f
Luca Miguel <luca.miguel@usp.br>
2023-06-29 12:28:26 +0200

    Added pipe switch

--- rocket_twin/systems/tank/pipe.py
+++ rocket_twin/systems/tank/pipe.py
@@ -8,6 +8,8 @@ class Pipe(System):
     ------
     w_in [kg/s]: float,
         mass flow of fuel entering the pipe
+    is_open: boolean,
+        whether the pipe is open or closed
 
     Outputs
     ------
@@ -17,8 +19,13 @@ class Pipe(System):
 
     def setup(self):
 
+        self.add_inward("is_open", False, desc="Whether the pipe is open or not", unit='')
         self.add_inward("w_in", 0.0, desc="Fuel income rate", unit="kg/s")
+
         self.add_outward("w_out", 0.0, desc="Fuel exit rate", unit="kg/s")
 
     def compute(self):
+
+        if self.is_open == False:
+            self.parent.g_tank.w_out_temp = 0.
         self.w_out = self.w_in
