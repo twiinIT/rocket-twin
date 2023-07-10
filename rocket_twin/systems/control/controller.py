87ed634f638185e49e13604de90a43827b18439d
Luca Miguel <luca.miguel@usp.br>
2023-07-10 14:21:44 +0200

    pre-commit

--- rocket_twin/systems/control/controller.py
+++ rocket_twin/systems/control/controller.py
@@ -1,8 +1,9 @@
 from cosapp.base import System
 from cosapp_fmu.FMUsystem import FMUSystem
-from rocket_twin.systems.control import CosappController
 
+from rocket_twin.systems.control import CosappController
 from rocket_twin.utils import create_FMU
+
 
 class Controller(System):
     """Controller of the command variables.
@@ -26,16 +27,21 @@ class Controller(System):
 
     def setup(self, model_path=None, model_name=None):
 
-        self.add_inward('time_var', 0., desc="System time", unit='')
-        self.add_transient('x', der='1')
+        self.add_inward("time_var", 0.0, desc="System time", unit="")
+        self.add_transient("x", der="1")
 
         if model_path is None:
-            self.add_child(CosappController('cos_control'), pulling=['f', 'wr', 'wg'])
+            self.add_child(CosappController("cos_control"), pulling=["f", "wr", "wg"])
         else:
             fmu_path = create_FMU(model_path, model_name)
-            self.add_child(FMUSystem("fmu_controller", fmu_path=fmu_path), pulling={'f' : 'f', 'wr' : 'wr', 'wg' : 'wg', 'ti' : 'time_var'})
+            self.add_child(
+                FMUSystem("fmu_controller", fmu_path=fmu_path),
+                pulling={"f": "f", "wr": "wr", "wg": "wg", "ti": "time_var"},
+            )
 
     def compute(self):
 
         self.time_var = self.time
-''
+
+
+""
--- rocket_twin/systems/control/cosapp_controller.py
+++ rocket_twin/systems/control/cosapp_controller.py
@@ -1,8 +1,9 @@
 from cosapp.base import System
 
+
 class CosappController(System):
     """System which allows command through CoSApp sequences.
-    
+
     Inputs
     ------
 
@@ -18,18 +19,16 @@ class CosappController(System):
 
     def setup(self):
 
-        self.add_inward('f_temp', 0., desc="Ratio of command force to maximum force", unit='')
-        self.add_inward('wr_temp', 0., desc="Ratio of command fuel flow to maximum flow", unit='')
-        self.add_inward('wg_temp', 0., desc="Ratio of command fuel flow to maximum flow", unit='')
+        self.add_inward("f_temp", 0.0, desc="Ratio of command force to maximum force", unit="")
+        self.add_inward("wr_temp", 0.0, desc="Ratio of command fuel flow to maximum flow", unit="")
+        self.add_inward("wg_temp", 0.0, desc="Ratio of command fuel flow to maximum flow", unit="")
 
-        self.add_outward('f', 0., desc="Ratio of command force to maximum force", unit='')
-        self.add_outward('wr', 0., desc="Ratio of command fuel flow to maximum flow", unit='')
-        self.add_outward('wg', 0., desc="Ratio of command fuel flow to maximum flow", unit='')
+        self.add_outward("f", 0.0, desc="Ratio of command force to maximum force", unit="")
+        self.add_outward("wr", 0.0, desc="Ratio of command fuel flow to maximum flow", unit="")
+        self.add_outward("wg", 0.0, desc="Ratio of command fuel flow to maximum flow", unit="")
 
     def compute(self):
 
         self.f = self.f_temp
         self.wr = self.wr_temp
         self.wg = self.wg_temp
-
-    
