df88f7df33145c58776795bb966eb008e1e1d321
Luca Miguel <luca.miguel@usp.br>
2023-07-13 12:46:08 +0200

    create_fmu is now a function of controllerFMU

--- rocket_twin/systems/control/controller_fmu.py
+++ rocket_twin/systems/control/controller_fmu.py
@@ -1,7 +1,10 @@
+import os
+
 from cosapp.base import System
 from cosapp_fmu.FMUsystem import FMUSystem
+from OMPython import ModelicaSystem
 
-from rocket_twin.utils import create_FMU
+import rocket_twin.systems.control
 
 
 class ControllerFMU(System):
@@ -29,7 +32,7 @@ class ControllerFMU(System):
         self.add_inward("time_var", 0.0, desc="System time", unit="")
         self.add_transient("x", der="1")
 
-        fmu_path = create_FMU(model_path, model_name)
+        fmu_path = self.create_fmu(model_path, model_name)
         self.add_child(
             FMUSystem("fmu_controller", fmu_path=fmu_path),
             pulling={"w": "w", "ti": "time_var"},
@@ -39,5 +42,35 @@ class ControllerFMU(System):
 
         self.time_var = self.time
 
+    def create_fmu(self, model_path, model_name):
+        """Create an fmu file in the control folder from an mo file.
 
-""
+        Inputs
+        ------
+        model_path: string
+            the path of the .mo file
+        model_name: string
+            the name of the .fmu file to be created
+
+        Outputs
+        ------
+
+        fmu: string
+            the path to the .fmu file
+        """
+
+        fmu_path = os.path.join(rocket_twin.systems.control.__path__[0], model_name)
+        model_path = os.path.join(rocket_twin.__path__[0], model_path)
+        model_path = model_path.replace("\\", "/")
+        try:
+            os.mkdir(fmu_path)
+        except OSError:
+            pass
+        os.chdir(fmu_path)
+        mod = ModelicaSystem(model_path, model_name)
+        fmu = mod.convertMo2Fmu()
+        for filename in os.listdir(fmu_path):
+            if filename != (model_name + ".fmu"):
+                os.remove(filename)
+
+        return fmu
--- rocket_twin/utils/__init__.py
+++ rocket_twin/utils/__init__.py
@@ -1,4 +1,3 @@
-from rocket_twin.utils.create_fmu import create_FMU
 from rocket_twin.utils.run_sequences import run_sequences
 
-__all__ = ["run_sequences", "create_FMU"]
+__all__ = ["run_sequences"]
