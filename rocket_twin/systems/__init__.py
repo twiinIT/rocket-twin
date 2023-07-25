4789d8075645192a3b4d5fd242e42dcf86fa854c
Luca Miguel <63428345+luca7084@users.noreply.github.com>
2023-07-25 13:49:20 +0200

    Two stages (#19)
    * Added stage class
    
    * pre-commit
    
    * Added unit test
    
    * pre-commit
    
    * CI test
    
    * pre-commit update
    
    * CI test

--- rocket_twin/systems/__init__.py
+++ rocket_twin/systems/__init__.py
@@ -4,13 +4,14 @@ from rocket_twin.systems.ground import Ground
 from rocket_twin.systems.physics import Dynamics
 from rocket_twin.systems.tank import Pipe, Tank
 
-from rocket_twin.systems.rocket import Rocket  # isort: skip
+from rocket_twin.systems.rocket import Stage, Rocket  # isort: skip
 from rocket_twin.systems.station import Station  # isort: skip
 
 __all__ = [
     "Clock",
     "Engine",
     "Tank",
+    "Stage",
     "Rocket",
     "Pipe",
     "Dynamics",
---
+++ rocket_twin/systems/rocket/stage.py
@@ -0,0 +1,39 @@
+from cosapp.base import System
+
+from rocket_twin.systems import ControllerCoSApp, Engine, Tank
+
+
+class Stage(System):
+    """Model of a rocket stage.
+
+    Inputs
+    ------
+
+    Outputs
+    ------
+    force [N]: float,
+        thrust force
+    weight [kg]: float,
+        weight
+    cg [m]: float,
+        center of gravity
+    """
+
+    def setup(self):
+
+        self.add_child(ControllerCoSApp("controller"))
+        self.add_child(Tank("tank"), pulling=["w_in", "weight_max", "weight_p"])
+        self.add_child(Engine("engine"))
+
+        self.connect(self.controller.outwards, self.tank.inwards, {"w": "w_command"})
+        self.connect(self.tank.outwards, self.engine.inwards, {"w_out": "w_out"})
+
+        self.add_outward("weight", 1.0, desc="Weight", unit="kg")
+        self.add_outward("cg", 1.0, desc="Center of gravity", unit="m")
+
+    def compute(self):
+
+        self.weight = self.tank.weight + self.engine.weight
+        self.cg = (self.tank.cg * self.tank.weight + self.engine.cg * self.engine.weight) / (
+            self.weight
+        )
