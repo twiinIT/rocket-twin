efe1041fa9cd3deac8094775e569bc3475bf5ec3
Luca Miguel <luca.miguel@usp.br>
2023-08-09 17:00:05 +0200

    pre-commit

--- rocket_twin/tests/test_nose.py
+++ rocket_twin/tests/test_nose.py
@@ -1,25 +1,33 @@
 import numpy as np
+
 from rocket_twin.systems import Nose
+
 
 class TestNose:
     """Tests for the nose model."""
 
     def test_geometry(self):
-        sys = Nose('sys')
+        sys = Nose("sys")
 
-        sys.radius = 5.
-        sys.height = 9.
-        sys.rho = 10.
-        sys.pos = 0.
+        sys.radius = 5.0
+        sys.height = 9.0
+        sys.rho = 10.0
+        sys.pos = 0.0
 
         sys.run_once()
 
-        np.testing.assert_allclose(sys.props.Mass(), 750*np.pi, atol=10**(-2))
+        np.testing.assert_allclose(sys.props.Mass(), 750 * np.pi, atol=10 ** (-2))
 
-        np.testing.assert_allclose(sys.props.CentreOfMass().X(), 0., atol=10**(-2))
-        np.testing.assert_allclose(sys.props.CentreOfMass().Y(), 0., atol=10**(-2))
-        np.testing.assert_allclose(sys.props.CentreOfMass().Z(), 2.25, atol=10**(-2))
+        np.testing.assert_allclose(sys.props.CentreOfMass().X(), 0.0, atol=10 ** (-2))
+        np.testing.assert_allclose(sys.props.CentreOfMass().Y(), 0.0, atol=10 ** (-2))
+        np.testing.assert_allclose(sys.props.CentreOfMass().Z(), 2.25, atol=10 ** (-2))
 
-        np.testing.assert_allclose(sys.props.MatrixOfInertia().Diagonal().X(), 6.7875*750*np.pi, atol=10**(-2))
-        np.testing.assert_allclose(sys.props.MatrixOfInertia().Diagonal().Y(), 6.7875*750*np.pi, atol=10**(-2))
-        np.testing.assert_allclose(sys.props.MatrixOfInertia().Diagonal().Z(), 7.5 *750*np.pi, atol=10**(-2))
+        np.testing.assert_allclose(
+            sys.props.MatrixOfInertia().Diagonal().X(), 6.7875 * 750 * np.pi, atol=10 ** (-2)
+        )
+        np.testing.assert_allclose(
+            sys.props.MatrixOfInertia().Diagonal().Y(), 6.7875 * 750 * np.pi, atol=10 ** (-2)
+        )
+        np.testing.assert_allclose(
+            sys.props.MatrixOfInertia().Diagonal().Z(), 7.5 * 750 * np.pi, atol=10 ** (-2)
+        )
--- rocket_twin/tests/test_tank.py
+++ rocket_twin/tests/test_tank.py
@@ -34,18 +34,24 @@ class TestTank:
         np.testing.assert_allclose(sys.weight_p, 0.0, atol=10 ** (-10))
 
     def test_geometry(self):
-        sys = Tank('sys')
+        sys = Tank("sys")
         driver = sys.add_driver(RungeKutta(order=4, dt=0.1))
         driver.time_interval = (0, 5)
 
-        init = {'geom.r_int' : 3., 'geom.r_ext' : 4., 'geom.thickness' : 0.1, 'geom.height' : 1.,
-                'geom.rho_struct' : 0.1, 'geom.rho_fuel' : 0.2, 'weight_p' : 6., 'w_in' : 0., 'fuel.w_out_max' : 1.}
-        
-        driver.set_scenario(init = init)
+        init = {
+            "geom.r_int": 3.0,
+            "geom.r_ext": 4.0,
+            "geom.thickness": 0.1,
+            "geom.height": 1.0,
+            "geom.rho_struct": 0.1,
+            "geom.rho_fuel": 0.2,
+            "weight_p": 6.0,
+            "w_in": 0.0,
+            "fuel.w_out_max": 1.0,
+        }
+
+        driver.set_scenario(init=init)
 
         sys.run_drivers()
 
-        np.testing.assert_allclose(sys.props.Mass(), 0.86 * np.pi + 1., atol=10**(-2))
-        
-
-
+        np.testing.assert_allclose(sys.props.Mass(), 0.86 * np.pi + 1.0, atol=10 ** (-2))
