2a8f9f3a16d51a4f1213c3d287feed4483ecce69
marcpog <145841769+marcpog@users.noreply.github.com>
2023-10-24 12:32:20 +0000

    commit 1 branch dev

---
+++ rocket_twin/ball_to_space.py
@@ -0,0 +1,33 @@
+from cosapp.base import System
+from cosapp.drivers import RungeKutta
+from cosapp.recorders import DataFrameRecorder
+import numpy as np
+
+class Ball(System):
+
+    def setup(self):
+
+        # 
+        self.add_inward('a0', np.array([2,2]), desc='acceleration value', unit='m/s**2')
+        self.add_outward('a', np.array([6,6]), desc='acceleration', unit='m/s**2')
+
+        self.add_transient('v', der='a', desc='velocity')
+        self.add_transient('r', der = 'v', desc = 'position')
+
+    def compute(self):
+
+        self.a = self.a_0
+
+
+ball = Ball('ball')
+
+driver = ball.add_drivers(RungeKutta(order=4,dt=0.1))
+driver.time_interval=(0, 10)
+driver.add_recorder(DataFrameRecorder(includes=['a','v','r']))
+
+ball.run_drivers()
+
+data = driver.recorder.export_data()
+print(data)
+
+#gitfetch
