343f8b8c023fa02469d398f4b200f45f92acdec5
marcpog <145841769+marcpog@users.noreply.github.com>
2023-10-24 14:20:16 +0000

    corrections

--- rocket_twin/tuto_marc_val/ball_to_space.py
+++ rocket_twin/tuto_marc_val/ball_to_space.py
@@ -8,20 +8,20 @@ class Ball(System):
     def setup(self):
 
         # 
-        self.add_inward('a0', np.array([2,2]), desc='acceleration value', unit='m/s**2')
-        self.add_outward('a', np.array([6,6]), desc='acceleration', unit='m/s**2')
+        self.add_inward('a0', np.array([2.0,2.0]), desc='acceleration value', unit='m/s**2')
+        self.add_outward('a', np.array([6.0,6.0]), desc='acceleration', unit='m/s**2')
 
         self.add_transient('v', der='a', desc='velocity')
         self.add_transient('r', der = 'v', desc = 'position')
 
     def compute(self):
 
-        self.a = self.a_0
+        self.a = self.a0
 
 
 ball = Ball('ball')
 
-driver = ball.add_drivers(RungeKutta(order=4,dt=0.1))
+driver = ball.add_driver(RungeKutta(order=4,dt=0.1))
 driver.time_interval=(0, 10)
 driver.add_recorder(DataFrameRecorder(includes=['a','v','r']))
 
