faebad6f6e098091d151b384363998f29d4a0a3c
marcpog <145841769+marcpog@users.noreply.github.com>
2023-11-16 15:26:38 +0000

    correction ofr design_mthod

--- rocket_twin/tuto_marc_val/pipe_flow.py
+++ rocket_twin/tuto_marc_val/pipe_flow.py
@@ -9,8 +9,8 @@ from cosapp.drivers import NonLinearSolver, RunSingleCase
 class connections_pipes(Port):
 
     def setup(self):
-        self.add_variable('P', desc = 'pressure value', unit = 'Pa')
-        self.add_variable('Q', desc='mass flow rate', unit = 'kg/s')
+        self.add_variable('P',1.0e5, desc = 'pressure value', unit = 'Pa')
+        self.add_variable('Q',1., desc='mass flow rate', unit = 'kg/s')
         #self.add_variable('k', desc='loss coefficient', unit = 'N*s/(kg*m**2)')
 
 
@@ -24,24 +24,26 @@ class Pipe(System):
 
         self.add_output(connections_pipes,'OUT')
         #self.add_outward('dp',2*10e5)
+        self.add_outward('dp',0.0)
 
         # design methods
-        self.add_inward('k_design', 2e5)
-        self.add_design_method('kd').add_unknown('k').add_equation(' OUT.P - IN.P == k_design')
-
+        self.add_inward('dp_design',2e5)
+        self.add_design_method('kd').add_unknown('k').add_equation(' dp == dp_design')  #allow the solver to modify the inward value
+        #init
+        #self.IN.P = 5e5
 
     def compute(self):
         
-        self.IN.P = 5e5
-        self.OUT.P = self.k*(self.IN.Q)**2 + self.IN.P
+        self.dp = self.k*(self.IN.Q)**2 
+        self.OUT.P = self.IN.P -self.dp 
         self.OUT.Q = self.IN.Q
 
 h2 = Pipe('H2')
-#h2.OUT.P = 4e5
-h2.IN.Q = 2
+h2.IN.Q = 2.
+h2.IN.P = 3.0e5
 #h2.run_once()
 
-solver = h2.add_driver(NonLinearSolver('solver', tol=1e-12))
+solver = h2.add_driver(NonLinearSolver('solver'))
 
 # Add design point
 case = solver.add_child(RunSingleCase('case'))
@@ -70,6 +72,11 @@ class HeadPipe(System):
         self.add_child(Pipe('pipe1'), pulling={'IN':'IN','k':'k1'})
         self.add_child(Pipe('pipe2'), pulling = {'OUT':'OUT','k':'k2'})
         self.connect(self.pipe1.OUT, self.pipe2.IN)
+
+        self.add_inward('dp_design',1.0e5)
+        self.add_design_method('kdd').add_unknown(['k1','k2']).add_equation([' IN.P - OUT.P == dp_design','pipe1.dp == 2*pipe2.dp'])
+
+
 
 
 
