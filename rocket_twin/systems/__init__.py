32714ec6ff21b6478cfa37f6a128d88d41248405
marcpog <145841769+marcpog@users.noreply.github.com>
2023-12-02 10:59:57 +0000

    minor bugs

--- rocket_twin/systems/__init__.py
+++ rocket_twin/systems/__init__.py
@@ -4,13 +4,13 @@ from rocket_twin.systems.control import (
     StationControllerCoSApp,
 )
 from rocket_twin.systems.engine import Engine, EngineGeom, EnginePerfo
-from rocket_twin.systems.ground import Ground
 from rocket_twin.systems.physics import Dynamics
 from rocket_twin.systems.structure import NoseGeom, TubeGeom, WingsGeom
 from rocket_twin.systems.tank import Pipe, Tank, TankFuel, TankGeom
 
 from rocket_twin.systems.rocket import OCCGeometry, Stage, Rocket  # isort: skip
 from rocket_twin.systems.station import Station  # isort: skip
+from rocket_twin.systems.ground import Ground # isort: skip
 
 __all__ = [
     "Engine",
--- rocket_twin/systems/physics/dynamics.py
+++ rocket_twin/systems/physics/dynamics.py
@@ -34,12 +34,12 @@ class Dynamics(System):
         self.add_property("weights", weights)
 
         # inwards
-        self.add_inward("pos", np.r[0., 0., 6400.0e3], desc="local position", unit="m")
+        self.add_inward("pos", np.array([0., 0., 6400.0e3]), desc="local position", unit="m")
         self.add_inward("g0", -10.0, desc="Gravity on earth", unit="m/s**2")
         self.add_inward('r0', 6400.0e3, desc="Radius of the Earth", unit="m")
 
         # outwards
-        self.add_outward("g", np.r_[0., 0., -10.0], desc="Gravity", unit="m/s**2")
+        self.add_outward("g", np.array([0., 0., -10.0]), desc="Gravity", unit="m/s**2")
         self.add_outward("a", 0.0, desc="Acceleration", unit="m/s**2")
 
         for weight in self.weights:
